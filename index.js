18f0cf24ac6bd3f4ceac2a4813a8eb291d6a4e50
darrylgibbs <services@dgibbs.me>
2025-12-15 09:47:48 -0300

    add new posts functionality

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const posts = [];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { posts });
 });
 
 app.get("/contact", (req, res) => {
@@ -16,14 +17,24 @@ app.get("/contact", (req, res) => {
 });
 
 // NEEDS TO READ THE POST AND HAVE IT APPEAR ON THE PAGE BELOW THE WRITING AREA
-app.post("/submitpost", (req, res) => {
-    res.render("index.ejs");
+app.post("/posts", (req, res) => {
+    //const { postTitle, postBody } = req.body;
+    const postTitle = req.body["postTitle"];
+    const postBody = req.body["postBody"];
+    posts.unshift({             // unshift makes the latest post go to the top
+        newPostTitle: postTitle,
+        newPostBody: postBody,
+        createdAt: new Date()
+    });    
+    res.redirect("/");
+
+    // EDITING AND DELETING POSTS REQUIRE EVEN LISTENERS --------------------------------------------    <<<<<<<<<<<<<<<<<
 });
 
 app.post("/submit", (req, res) => {
     const contactResponse = "Thanks " + req.body["name"] + "! We will email you shortly at " + req.body["email"] + " and respond to your message.";
     res.render("contact.ejs", { emailSent: contactResponse});
-    sconsole.log(req.body);
+    console.log(req.body);
 });
 
 app.listen(port, () => {
